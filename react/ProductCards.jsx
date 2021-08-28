6284586db9c710dc30fae1b93213eaee16ce71ee
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-08-28 18:32:33 -0300

    Created the custom product

---
+++ react/ProductCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ProductCards.css'
+
+const ProductCards = (props) => {
+    return (
+        <div className={styles.productCard}>ProductCard</div>
+    )
+}
+
+export default ProductCards
