aae38a4edb8ae03605cbfe11d3bdeebeb049bba1
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-08-29 01:52:37 -0300

    Finished the base card

--- react/ProductCards.jsx
+++ react/ProductCards.jsx
@@ -1,47 +1,112 @@
 import React from "react"
 import styles from "./ProductCards.css"
 
-import { RiRocketLine } from "react-icons/ri"
 import { FiAlertCircle } from "react-icons/fi"
 import { FiCheck } from "react-icons/fi"
 import { FiX } from "react-icons/fi"
 
+import { FiActivity } from "react-icons/fi"
+import { FiCloudDrizzle } from "react-icons/fi"
+import { FiCommand } from "react-icons/fi"
+
+
+
+const testData = [
+    {
+        title: "CRO",
+        description: "Data Analytics com foco em CRO",
+        price: "349.99",
+        Icon: FiActivity,
+        caracteristics: [
+            { label: "Estudo de Conversão", status: true },
+            { label: "Elaboração de Dashboard", status: false },
+            {
+                label: "Participações em discussões de estratégia",
+                status: false,
+            },
+            { label: "Configuração do Analytics", status: false },
+        ],
+    },
+    {
+        title: "GOLIVE",
+        description:
+            "Acompanhar os principais ofensores e oportunidades do Funil",
+        price: "549.99",
+        Icon: FiCloudDrizzle,
+        caracteristics: [
+            { label: "Estudo de Conversão", status: true },
+            { label: "Elaboração de Dashboard", status: true },
+            {
+                label: "Participações em discussões de estratégia",
+                status: false,
+            },
+            { label: "Configuração do Analytics", status: true },
+        ],
+    },
+    {
+        title: "TESTE AB",
+        description:
+            "Realizar Testes entre Layouts, Versões de Site, Landing Pages",
+        price: "1099.99",
+        Icon: FiCommand,
+        caracteristics: [
+            { label: "Estudo de Conversão", status: true },
+            { label: "Elaboração de Dashboard", status: true },
+            {
+                label: "Participações em discussões de estratégia",
+                status: true,
+            },
+            { label: "Configuração do Analytics", status: true },
+        ],
+    },
+]
+
 const ProductCards = (props) => {
     return (
         <div className={styles.mainProductCardContainer}>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {testData.map((item) => {
+                return <ProductCard {...item} />
+            })}
         </div>
     )
 }
 
-const ProductCard = (props) => {
+const ProductCard = ({ title, description, price, caracteristics, Icon }) => {
     return (
         <div className={styles.productCard}>
-            <RiRocketLine
+            <Icon
                 className={styles.marginBottom}
                 size={60}
                 color="#fcc200"
             />
-            <h2>Starter</h2>
-            <span className={styles.subTitle}>
-                Gestão e consulta de NFes e CTes para até 3 CNPJs
-            </span>
+            <h2>{title}</h2>
+            <div
+                style={{
+                    height: "60px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <span className={styles.subTitle}>{description}</span>
+            </div>
             <span className={styles.subTitlePrice}>A partir de</span>
-            <label className={styles.pricing}>R$ 39,90</label>
-            <span className={styles.subTitlePrice}>Por més</span>
+            <label className={styles.pricing}>{"R$ " + price}</label>
+            <span className={styles.subTitlePrice}>Por mês</span>
             <button className={styles.productCardBuyButton}>
                 Assinar agora
             </button>
             <span className={styles.productCaracteristics}>
                 Principais recursos
             </span>
-            <ProductCaracteristic label="Estudo de Conversão" status={true} />
-            <ProductCaracteristic label="Elaboração de Dashboard" status={true} />
-            <ProductCaracteristic label="Participações em discussões de estratégia" status={true} />
-            <ProductCaracteristic label="Mensuração do cenário de CRO" status={true} />
-            <ProductCaracteristic label="Configuração do Analytics" status={true} />
+            {caracteristics.map((item) => {
+                return (
+                    <ProductCaracteristic
+                        label={item.label}
+                        status={item.status}
+                    />
+                )
+            })}
         </div>
     )
 }
