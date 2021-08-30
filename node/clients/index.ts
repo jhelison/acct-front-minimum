aa1e46c7d35b18e5aeef64a2307d0a0ba8a4589b
Alessandro Amelio <alessandro.amelio@kplay.com.br>
2021-08-30 14:51:01 -0300

    Fix de Integracao

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -2,25 +2,18 @@ import { IOClients } from '@vtex/api'
 
 import { OMS, Catalog } from '@vtex/clients'
 
-import Status from './status'
-import Leads from './leads'
-import LeadByEmail from './leadByEmail'
+// import Status from './status'
+// import Leads from './leads'
+// import LeadByEmail from './leadByEmail'
 import leadCLI from './leadCLI'
-import SkuByProductId from './skuByProductId'
-import ProductById from './productById'
-import SpecificationsByProductId from './specificationsByProductId'
+// import SkuByProductId from './skuByProductId'
+// import ProductById from './productById'
+// import SpecificationsByProductId from './specificationsByProductId'
 
 import VtexApi from './vtexapi'
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
-
-  public get status() {
-    return this.getOrSet('status', Status)
-  }
-
-export class Clients extends IOClients {
-
 
   public get leadCLI() {
     return this.getOrSet('leadCLI', leadCLI)
